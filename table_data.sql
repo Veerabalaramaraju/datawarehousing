cbfd2108573d43977bb689921f0a070410d23d53
Ramaraju <veerabalaramaraju1045@gmail.com>
2025-10-31 12:49:43 +0530

    Adding Values into the table

---
+++ table_data.sql
@@ -0,0 +1,5 @@
+# Student database
+INSERT INTO Student (student_id, name, number) VALUES (1,'Ramaraju','9876543210'),(2,'Hari','9123456789'),(3,'Sai','9989901942'),
+(4,'Pratap','9988776655');
+
+INSERT INTO enrollment VALUES (101, 1, 'CS101'),(102, 1, 'MATH201'),(103, 2, 'CS101'),(104, 4, 'PHY301');
