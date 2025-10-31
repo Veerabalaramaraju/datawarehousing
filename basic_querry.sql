9158a71b44281f1af1b50cfcfba95d7b10cf9f03
Ramaraju <veerabalaramaraju1045@gmail.com>
2025-10-31 12:55:56 +0530

    Basic SQL Queery

---
+++ basic_querry.sql
@@ -0,0 +1,17 @@
+# Basic Select Querry
+
+select * from student;
+
+"student_id"	"name"	"number"
+1	"Ramaraju"	"9876543210"
+2	"Hari"	"9123456789"
+3	"Sai"	"9989901942"
+4	"Pratap"	"9988776655"
+
+select * from enrollment;
+
+"entrollmet_id"	"student_id"	"course_code"
+101	1	"CS101"
+102	1	"MATH201"
+103	2	"CS101"
+104	4	"PHY301"
